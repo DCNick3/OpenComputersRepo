eb49dd62788d684b10201c826020baa4f0a094bf
DCNick3 <nikita@bk.ru>
2016-11-15 21:18:56 +0300

    Grammmatical mistake =)

--- index_pretty.lua
+++ index_pretty.lua
@@ -1,8 +1,8 @@
 return {
- ["test-library"]={versions={"1.0"},author="DCNick3",dependecies={
+ ["test-library"]={versions={"1.0"},author="DCNick3",dependencies={
   ["1.0"]={} }
   },
- ["test-program"]={versions={"1.0"},author="DCNick3",dependecies={
+ ["test-program"]={versions={"1.0"},author="DCNick3",dependencies={
   ["1.0"]={} }
  }
 }
